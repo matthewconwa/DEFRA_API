b9d5637be11acc9aaff9eacb5cbdd37e1c1ff5a0
Mateusz Lasak <mateuszunix@gmail.com>
2023-06-20 21:03:28 +0200

    4th commit

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
-import requests, re
-"""
+import requests
+
 class WrongPokemonName(Exception):
     pass
 
+def request_json(webPage):
 
-def user_input_dictionary():
-    req = requests.get(f"https://environment.data.gov.uk/hydrology/id/stations")
+    req = requests.get(webPage)
     req = req.json()
 
-    return dict(req)
+    return req
+
+def get_dictionary(jsonRequest):
+
+    #req = requests.get("https://environment.data.gov.uk/hydrology/id/stations")
+    #req = req.json()
+
+    dictionary = {}
+    label: str
+    stationGuid: str
+
+    for line in jsonRequest['items']:
+        for key, value in dict(line).items():
+            if key == 'stationGuid':
+                stationGuid = value
+            elif key == 'label':
+                label = value
+
+        dictionary.update({f'{label}': stationGuid})
+
+    return dictionary
 
 
-def user_input():
+def user_input(dicOfStation):
     while True:
         try:
             answer = str(input("Please provide ID of station: "))
@@ -32,91 +52,28 @@ def user_input():
             break
     return answer
 
-def request_json(pokemon):
-
-    req = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
-    pokemon = req.json()
-
-    return dict(pokemon)
-"""
 
 if __name__ == '__main__':
     pass
 
-#dicOfStation = user_input_dictionary()
+webPage = "https://environment.data.gov.uk/hydrology/id/stations"
 
-"""
-req = requests.get("https://environment.data.gov.uk/hydrology/id/stations")
-person = req.json()
+# fetching json request
+jsonRequest = request_json(webPage)
 
-for film in person['items']:
-    req = requests.get(film)
-    film = req.json()
-    print("Film is: ", film['@id'])
+# creation of dictionary of Station
+dicOfStation = get_dictionary(jsonRequest)
 
+#user input
+#user_input(dicOfStation)
 
-#answer = user_input()
-#print(answer)
-
-#pokeamon = request_json(answer)
-#print(pokeamon)
-#print(type(pokeamon))
-
-"""
-import json
-
-req = """
-{
-   "items":[
-      {
-         "@id":"http://environment.data.gov.uk/hydrology/id/stations/c46d1245-e34a-4ea9-8c4c-410357e80e15",
-         "nrfaStationID":"45007",
-         "type":[
-            {
-               "@id":"http://environment.data.gov.uk/flood-monitoring/def/core/Station"
-            },
-            {
-               "@id":"http://environment.data.gov.uk/reference/def/core/SamplingLocation"
-            }
-            ]
-
-      }
-   ]
-}"""
-
-person = json.loads(req)
-
-"""
-for line in person['book']:
-    d = dict(line)
+for key, value in dicOfStation.items():
+    print(key, value)
 
 
-for k, v in d.items():
-    print(k, v)
 
-for line in person['items']:
-    for key, value in dict(line).items():
-        print(key, value)
 
-"""
-req = requests.get("https://environment.data.gov.uk/hydrology/id/stations")
-req = req.json()
 
-dictionary = {}
-label: str
-stationGuid: str
-
-for line in req['items']:
-    for key, value in dict(line).items():
-        if key == 'stationGuid':
-            stationGuid = value
-        elif key == 'label':
-            label = value
-
-    dictionary.update({f'{label}': stationGuid})
-
-for i, j in dictionary.items():
-    print(i, j)
 
 
 
