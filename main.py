76f4e041762d4c0f5b8cd3a7d93b4b9fc4dddb40
Mateusz Lasak <mateuszunix@gmail.com>
2023-06-20 19:40:35 +0200

    2nd commit

--- main.py
+++ main.py
@@ -1,16 +1,113 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import requests, re
+"""
+class WrongPokemonName(Exception):
+    pass
 
 
-# Press the green button in the gutter to run the script.
+def user_input_dictionary():
+    req = requests.get(f"https://environment.data.gov.uk/hydrology/id/stations")
+    req = req.json()
+
+    return dict(req)
+
+
+def user_input():
+    while True:
+        try:
+            answer = str(input("Please provide ID of station: "))
+            if answer in ("", " "):
+                raise TypeError
+            #if answer != "ditto":
+                #raise WrongPokemonName
+
+        except TypeError:
+            print("Sorry, you haven't provided the right ID of station")
+            continue
+
+        except WrongPokemonName:
+            print("You have choosen wrong Pokemon name")
+
+        else:
+            #let's break the loop, value has been iserted correctly
+            break
+    return answer
+
+def request_json(pokemon):
+
+    req = requests.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
+    pokemon = req.json()
+
+    return dict(pokemon)
+"""
+
 if __name__ == '__main__':
-    print_hi('PyCharm fefe')
+    pass
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#dicOfStation = user_input_dictionary()
+
+"""
+req = requests.get("https://environment.data.gov.uk/hydrology/id/stations")
+person = req.json()
+
+for film in person['items']:
+    req = requests.get(film)
+    film = req.json()
+    print("Film is: ", film['@id'])
+
+
+#answer = user_input()
+#print(answer)
+
+#pokeamon = request_json(answer)
+#print(pokeamon)
+#print(type(pokeamon))
+
+"""
+import json
+
+req = """
+{
+   "book": [
+
+      {
+         "id": "01",
+         "language": "Java",
+         "edition": "third",
+         "author": "Herbert Schildt"
+      },
+
+      {
+         "id": "07",
+         "language": "C++",
+         "edition": "second",
+         "author": "E.Balagurusamy"
+      }
+
+   ]
+}
+"""
+person = json.loads(req)
+
+"""
+for line in person['book']:
+    d = dict(line)
+
+
+for k, v in d.items():
+    print(k, v)
+"""
+
+for line in person['book']:
+    for key, value in dict(line).items():
+        print(key, value)
+
+
+
+
+"""
+print(person['book'])
+
+pattern_without_braces = r'(?<=\[).*?(?=\])'
+parts = re.findall(pattern_without_braces, str(person))
+print(parts)
+"""
