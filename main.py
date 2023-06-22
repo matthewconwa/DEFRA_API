6affb6ecc64cd1183092caaf3a8ef881a5a325b1
Mateusz Lasak <mateuszunix@gmail.com>
2023-06-22 20:46:48 +0200

    6th commit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import requests
-
-class WrongPokemonName(Exception):
+from datetime import datetime
+class WrongStation(Exception):
+    pass
+class WrongRangeChosen(Exception):
     pass
 
-class WrongStation(Exception):
+class WrongRange(Exception):
     pass
 
 def request_json(webPage):
@@ -30,8 +32,7 @@ def get_dictionary(jsonRequest):
 
     return dictionary
 
-
-def user_input(dicOfStation):
+def initial_user_input(dicOfStation):
     while True:
         try:
             answer = str(input("Please provide Station Name or ID of station: "))
@@ -52,16 +53,143 @@ def user_input(dicOfStation):
             continue
 
         else:
-            #let's break the loop, value has been iserted correctly
+            #let's break the loop, value has been inserted correctly
             break
     return answer
 
-def get_data():
-    pass
 
+def get_basic_data(answer, dicOfStation):
+
+    dictionary2 = {}
+    key_tmp: str
+    value_tmp: str
+    req: dict
+
+    # if user has provided name of station, this piece of code maps it to GUID based on dictionary
+    if answer not in dicOfStation.values():
+        for key, value in dicOfStation.items():
+            if answer == key:
+                answer = value
+
+    req = requests.get(f"https://environment.data.gov.uk/hydrology/id/stations/{answer}")
+    req = req.json()
+
+
+    for line in req['items']:
+        for key, value in dict(line).items():
+
+            if key == 'label':
+                value_tmp = value
+                dictionary2.update({f'{"label"}': value_tmp})
+
+            elif key == 'easting':
+                value_tmp = value
+                dictionary2.update({f'{"easting"}': value_tmp})
+
+            elif key == 'lat':
+                value_tmp = value
+                dictionary2.update({f'{"lat"}': value_tmp})
+
+            elif key == 'long':
+                value_tmp = value
+                dictionary2.update({f'{"lat"}': value_tmp})
+
+            elif key == 'wiskiID':
+                value_tmp = value
+                dictionary2.update({f'{"wiskiID"}': value_tmp})
+
+            elif key == 'gridReference':
+                value_tmp = value
+                dictionary2.update({f'{"gridReference"}': value_tmp})
+
+            elif key == 'searchText':
+                value_tmp = value
+                dictionary2.update({f'{"searchText"}': value_tmp})
+
+            elif key == 'riverName':
+                value_tmp = value
+                dictionary2.update({f'{"riverName"}': value_tmp})
+
+            elif key == 'northing':
+                value_tmp = value
+                dictionary2.update({f'{"northing"}': value_tmp})
+
+            elif key == 'dateOpened':
+                value_tmp = value
+                dictionary2.update({f'{"dateOpened"}': value_tmp})
+
+    print("\n Basic information about chosen station:\n")
+    for key, value in dictionary2.items():
+        print(f"{key}:     ", value)
+
+    return answer
+
+
+def get_date_range():
+    date_range = []
+
+    answer = '48513a18-e485-4317-ae92-93bf4f7f3e54'
+
+    req = requests.get(
+        f"https://environment.data.gov.uk/hydrology/id/measures/{answer}-flow-i-900-m3s-qualified/readings?earliest")
+    req = req.json()
+
+    for line in req['items']:
+        for key, value in dict(line).items():
+            if key == 'date':
+                date_range.append(value)
+
+    req = requests.get(
+        f"https://environment.data.gov.uk/hydrology/id/measures/{answer}-flow-i-900-m3s-qualified/readings?latest")
+    req = req.json()
+
+    for line in req['items']:
+        for key, value in dict(line).items():
+            if key == 'date':
+                date_range.append(value)
+
+    return date_range
+
+def get_detailed_information(date_range):
+
+    while True:
+        try:
+            print(f"\nFor the selected station, the date range is as follows: {date_range}")
+            usr_input = input(f"Please choose one of available dates i.e {min(date_range)}: ")
+
+            if usr_input in ("", " "):
+                raise TypeError
+
+            if not datetime.strptime(usr_input, '%Y-%m-%d'):
+                raise ValueError
+
+            if usr_input < min(date_range) or usr_input > max(date_range):
+                raise WrongRange
+
+        except TypeError:
+            print("Sorry, you haven't provided any information, please try once again...")
+            continue
+
+        except ValueError:
+            print("Sorry, you haven't provided date in the right format")
+            continue
+
+        except WrongRange:
+            print("Sorry, you haven't provided available date per chosen Station")
+            continue
+
+        else:
+            #let's break the loop, value has been inserted correctly
+            break
+
+    return usr_input
+
+
+
+#Main function, running program
 if __name__ == '__main__':
     pass
-"""
+
 webPage = "https://environment.data.gov.uk/hydrology/id/stations"
 
 # fetching json request
@@ -71,19 +199,50 @@ jsonRequest = request_json(webPage)
 dicOfStation = get_dictionary(jsonRequest)
 
 # getting data from user and checking if value inserted exists in dictionary
-user_input(dicOfStation)
+answer = initial_user_input(dicOfStation)
 
-#for key, value in dicOfStation.items():
-#    print(key, value)
+# getting basic information about particular station
+answer = get_basic_data(answer, dicOfStation)
+
+# getting range of available dates for chosen station
+date_range = get_date_range()
+
+
+"""
+print(f"\nFor the selected station, the date range is as follows: {date_range}")
+input = input(f"Please choose one of available dates i.e {min(date_range)}: ")
+
+answer = '48513a18-e485-4317-ae92-93bf4f7f3e54'
 """
 
-req = requests.get("https://environment.data.gov.uk/hydrology/id/stations")
+date_chosen = get_detailed_information(date_range)
+
+req = requests.get(
+    f"https://environment.data.gov.uk/hydrology/id/measures/{answer}-flow-i-900-m3s-qualified/readings?date={date_chosen}")
 req = req.json()
+
 
 for line in req['items']:
     for key, value in dict(line).items():
-        
         print(key, value)
+
+
+
+
+"""
+usr_input = '2022-01-01'
+
+try:
+    if datetime.strptime(usr_input, '%Y-%m-%d'):
+        print("dupa")
+
+except ValueError as err:
+    print("Sorry, you haven't provided available date per chosen Station")
+"""
+
+
+
+
 
 
 
